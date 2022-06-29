ad20010f6bfdee205e74d9aa12d201caf8d8b298
Romain Beauxis <toots@rastageeks.org>
2022-06-28 20:59:06 -0500

    First stab.

--- packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
@@ -13,14 +13,6 @@ cp compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlcommon.a \
    driver/optmain.cmx driver/optmain.o \
    "${PREFIX}/windows-sysroot/lib/ocaml/compiler-libs"
 
-for bin in ocamlc ocamlopt ocamlcp ocamlmklib ocamlmktop ocamldoc ocamldep; do
-  cat >"${PREFIX}/windows-sysroot/bin/${bin}" <<END
-#!/bin/sh
-${PREFIX}/bin/ocamlrun "${PREFIX}/windows-sysroot/bin/${bin}.exe" "\$@"
-END
-  chmod +x "${PREFIX}/windows-sysroot/bin/${bin}"
-done
-
 for pkg in bigarray bytes compiler-libs dynlink findlib graphics stdlib str threads unix; do
   if [ -d "${PREFIX}/lib/${pkg}" ]; then
     cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/windows-sysroot/lib/"
