eeb4ec04607afefd7d749c01f367dba27721eab7
Romain Beauxis <toots@rastageeks.org>
2023-01-29 18:43:56 -0600

    Update flexdll-windows

--- packages/ocaml-windows64/ocaml-windows64.4.14.0/files/build.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.14.0/files/build.sh
@@ -2,7 +2,6 @@
 
 HOST=$1
 OPAM_PREFIX="$2"
-FLEXDLL="$3"
 
 OPTS=""
 
@@ -10,10 +9,9 @@ if [ `opam var conf-flambda-windows:installed` = "true" ]; then
   OPTS="--enable-flambda"
 fi
 
-./configure --host=$1 --with-flexdll="${FLEXDLL}" --prefix="${OPAM_PREFIX}/windows-sysroot" ${OPTS}
+./configure --host=$1 --prefix="${OPAM_PREFIX}/windows-sysroot" --enable-systhreads ${OPTS}
 
-PWD=`pwd`
-CAMLC="${OPAM_PREFIX}/bin/ocamlc"
+CAMLC=`which ocamlc`
 
 make -C runtime sak.exe SAK_CC=cc SAK_CFLAGS= SAK_LINK='cc -o $(1) $(2)'
 make ocamlc ocamlopt OCAMLRUN=ocamlrun NEW_OCAMLRUN=ocamlrun CAMLC="${CAMLC}"
@@ -23,27 +21,23 @@ make -C runtime all libasmrun.a CAMLC="${CAMLC}"
 make -C stdlib \
      OCAMLRUN=ocamlrun \
      NEW_OCAMLRUN=ocamlrun \
-     CAMLC=ocamlc \
      CAMLC="${CAMLC}" \
-     COMPILER=ocamlc \
+     COMPILER="${CAMLC}" \
      OPTCOMPILER="${PWD}/ocamlopt.exe" \
      OCAMLOPT="${PWD}/ocamlopt.exe" \
      OTHERLIBRARIES="bigarray str win32unix systhreads" \
      MKLIB="ocamlrun \"${PWD}/tools/ocamlmklib.exe\"" \
-     FLEXLINK_CMD="${FLEXDLL}/flexlink" \
      OCAMLYACC=ocamlyacc
 
 make -C yacc \
      OCAMLRUN=ocamlrun \
      NEW_OCAMLRUN=ocamlrun \
-     CAMLC=ocamlc \
      CAMLC="${CAMLC}" \
-     COMPILER=ocamlc \
+     COMPILER="${CAMLC}" \
      OPTCOMPILER="${PWD}/ocamlopt.exe" \
      OCAMLOPT="${PWD}/ocamlopt.exe" \
      OTHERLIBRARIES="bigarray str win32unix systhreads" \
      MKLIB="ocamlrun \"${PWD}/tools/ocamlmklib.exe\"" \
-     FLEXLINK_CMD="${FLEXDLL}/flexlink" \
      OCAMLYACC=ocamlyacc \
      installed_tools=
 
@@ -57,13 +51,11 @@ make library \
      driver/optmain.cmx \
      OCAMLRUN=ocamlrun \
      NEW_OCAMLRUN=ocamlrun \
-     CAMLC=ocamlc \
      CAMLC="${CAMLC}" \
-     COMPILER=ocamlc \
+     COMPILER="${CAMLC}" \
      OPTCOMPILER="${PWD}/ocamlopt.exe" \
      OCAMLOPT="${PWD}/ocamlopt.exe" \
      OTHERLIBRARIES="bigarray str win32unix systhreads" \
      MKLIB="ocamlrun \"${PWD}/tools/ocamlmklib.exe\"" \
-     FLEXLINK_CMD="${FLEXDLL}/flexlink" \
      OCAMLYACC=ocamlyacc \
      installed_tools=
--- packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -e
 
 PREFIX="$1"
-FLEXDLL="$2"
-PWD=`pwd`
 
 touch \
   tools/profiling.cmo \
@@ -25,37 +23,33 @@ make install \
   RUNTIMED=false \
   INSTRUMENTED_RUNTIME=false \
   INSTALL_SOURCE_ARTIFACTS=false \
-  installed_tools= \
+  installed_tools=ocamlmklib \
   OTHERLIBRARIES="bigarray str win32unix systhreads" \
   programs= \
-  ocamldebugger= \
-  BOOTSTRAPPING_FLEXDLL=false
+  ocamldebugger=
+
+CAMLC=`which ocamlc`
 
 # Dynlink only compiles after the above has been installed
 make otherlibraries opt \
   OCAMLRUN=ocamlrun \
   NEW_OCAMLRUN=ocamlrun \
-  CAMLC=ocamlc \
   CAMLC="${CAMLC}" \
-  COMPILER=ocamlc \
   OPTCOMPILER="${PWD}/ocamlopt.exe" \
   OCAMLOPT="${PWD}/ocamlopt.exe" \
   OTHERLIBRARIES="bigarray dynlink str win32unix systhreads" \
   MKLIB="ocamlrun \"${PWD}/tools/ocamlmklib.exe\"" \
-  FLEXLINK_CMD="${FLEXDLL}/flexlink" \
-  OCAMLYACC=ocamlyacc \
-  installed_tools=
+  OCAMLYACC=ocamlyacc
 
 make install \
   PROGRAMS=ocamlrun.exe \
   RUNTIMED=false \
   INSTRUMENTED_RUNTIME=false \
   INSTALL_SOURCE_ARTIFACTS=false \
-  installed_tools= \
+  installed_tools=ocamlmklib \
   OTHERLIBRARIES="bigarray dynlink str win32unix systhreads" \
   programs= \
-  ocamldebugger= \
-  BOOTSTRAPPING_FLEXDLL=false
+  ocamldebugger=
 
 cp compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlcommon.a \
    compilerlibs/ocamlbytecomp.cmxa compilerlibs/ocamlbytecomp.a \
