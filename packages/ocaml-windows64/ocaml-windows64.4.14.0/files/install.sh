9afbc778fa53b61691840de8628eb337b106d6e7
Romain Beauxis <toots@rastageeks.org>
2023-02-11 09:20:20 -0600

    Install META files.

--- packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.14.0/files/install.sh
@@ -58,5 +58,13 @@ cp compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlcommon.a \
    driver/optmain.cmx driver/optmain.o \
    "${PREFIX}/windows-sysroot/lib/ocaml/compiler-libs"
 
+# Copy META files from ocamlfind
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics stdlib str threads unix; do
+  if [ -f "${PREFIX}/lib/${pkg}/META" ]; then
+    mkdir -p "${PREFIX}/windows-sysroot/lib/${pkg}"
+    cp -r "${PREFIX}/lib/${pkg}/META" "${PREFIX}/windows-sysroot/lib/${pkg}/META"
+  fi
+done
+
 mkdir -p "${PREFIX}/lib/findlib.conf.d"
 cp windows.conf "${PREFIX}/lib/findlib.conf.d"
