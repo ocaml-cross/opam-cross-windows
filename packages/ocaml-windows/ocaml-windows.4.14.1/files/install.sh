1e2a30d4c2af05ace76ebebc7ccad024838a2512
Romain Beauxis <toots@rastageeks.org>
2023-02-12 14:35:52 -0600

    Add non exec paths.

--- packages/ocaml-windows/ocaml-windows.4.14.1/files/install.sh
+++ packages/ocaml-windows/ocaml-windows.4.14.1/files/install.sh
@@ -3,10 +3,11 @@
 PREFIX="$1"
 HOST="$2"
 
-for bin in ocamlopt ocamlmklib; do
+for bin in ocamlc ocamlopt ocamlmklib; do
   ln -s "${PREFIX}/windows-sysroot/bin/${bin}.exe" "${PREFIX}/bin/${HOST}-${bin}"
+  ln -s "${PREFIX}/windows-sysroot/bin/${bin}.exe" "${PREFIX}/windows-sysroot/bin/${bin}"
 done
 
-for bin in ocamlc ocamlcp ocamlmktop ocamldoc ocamldep; do
+for bin in ocamlcp ocamlmktop ocamldoc ocamldep; do
   ln -s "${PREFIX}/bin/${bin}" "${PREFIX}/bin/${HOST}-${bin}"
 done
