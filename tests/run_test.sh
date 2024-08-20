6ffb6b74de4ff285be6795402edd7b559cf77f90
Brian Ward <bward@flatironinstitute.org>
2024-08-20 12:38:16 -0400

    Add yojson.2.2.2, yojson-five.2.2.2 (#323)

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -23,7 +23,7 @@ printf "Building ${PACKAGE} using ${IMAGE}.. "
 
 DOCKER_CMD="docker build -f ${TEST_PWD}/Dockerfile.test --no-cache \
                 --build-arg \"PKG_CONFIG_PATH=${PKG_CONFIG_PATH}\" \
-                --build-arg \"IMAGE=${IMAGE}\" --build-arg \"OPAM_PKG=${PACKAGE}\" . 2>/dev/null"
+                --build-arg \"IMAGE=${IMAGE}\" --build-arg \"OPAM_PKG=${PACKAGE}\" . "
 
 PACKAGE_DIR=`echo ${PACKAGE} | cut -d'.' -f 1`
 
@@ -34,7 +34,7 @@ else
     echo ""
     /bin/sh -c "${DOCKER_CMD}"
   else
-    /bin/sh -c "${DOCKER_CMD} >/dev/null"
+    /bin/sh -c "${DOCKER_CMD} >/dev/null 2>/dev/null"
   fi
 
   if [ "$?" -ne "0" ]; then
@@ -50,13 +50,13 @@ if [ -n "${REVDEPS}" ]; then
   printf "Building ${PACKAGE} reverse dependencies using ${IMAGE}.. "
 
   DOCKER_CMD="docker build -f ${TEST_PWD}/Dockerfile.revdeps --no-cache \
-                  --build-arg \"IMAGE=${IMAGE}\" --build-arg \"OPAM_PKG=${PACKAGE}\" . 2>/dev/null"
+                  --build-arg \"IMAGE=${IMAGE}\" --build-arg \"OPAM_PKG=${PACKAGE}\" . "
 
   if [ -n "${VERBOSE}" ]; then
     echo ""
     /bin/sh -c "${DOCKER_CMD}"
   else
-    /bin/sh -c "${DOCKER_CMD} >/dev/null"
+    /bin/sh -c "${DOCKER_CMD} >/dev/null 2>/dev/null"
   fi
 
   if [ "$?" -ne "0" ]; then
