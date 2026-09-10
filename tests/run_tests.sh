1ae6744c18b766088210df7d6d03301534c1da99
Brian Ward <brianmward99@gmail.com>
2026-09-09 20:17:40 -0400

    + ocaml-windows.5.5.1 (#412)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,7 +6,7 @@ TEST_PWD=$(cd `dirname $0` && pwd)
 BASE_PWD=$(cd ${TEST_PWD}/.. && pwd)
 
 if [ -z "${OCAML_VERSION}" ]; then
-  OCAML_VERSION=5.5.0
+  OCAML_VERSION=5.5.1
 fi
 
 COMPILER="${OCAML_VERSION}"
